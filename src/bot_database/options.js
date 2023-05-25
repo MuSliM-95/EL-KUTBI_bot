6fa5dc57da2491d95d50111da39071a6d9ffb7a6
AgReSSoR <samahkinets@gmail.com>
2023-05-25 14:10:40 +0300

    Заменил ссылку на сайт

--- src/bot_database/options.js
+++ src/bot_database/options.js
@@ -1,45 +1,40 @@
-
 function key() {
-
-
-    return keyboardСontainer = {
-        options: {
-            parse_mode: 'HTML',
-            reply_markup: {
-                keyboard: [
-                    [{ text: 'و عليكم السلام ورحمة الله وبركاته' }],
-                    [{ text: 'Доставка и цены' }],
-                    [{ text: 'Оформить заказ', web_app: {url:'https://el-kutbi-frontend.onrender.com/'} }],
-                    [{ text: 'Связаться с поддержкой' }],
-                    [{ text: 'Закрыть помощника' }]
-
-                ]
+  return (keyboardСontainer = {
+    options: {
+      parse_mode: "HTML",
+      reply_markup: {
+        keyboard: [
+          [{ text: "و عليكم السلام ورحمة الله وبركاته" }],
+          [{ text: "Доставка и цены" }],
+          [
+            {
+              text: "Оформить заказ",
+              web_app: { url: "http://45.12.74.138:3000/" },
             },
-            closeTheKeyboard: {
-                reply_markup: {
-                    remove_keyboard: true
-                }
-            }
-        }, 
-
-        admin_keyboardСontainer: {
-            parse_mode: 'HTML',
- 
-            reply_markup: {
-                keyboard: [
-                    [{ text: 'و عليكم السلام ورحمة الله وبركاته' }],
-                    [{ text: 'Доставка и цены' }],
-                    [{ text: 'Закрыть помощника' }],
-                ]
-            },
-
-
+          ],
+          [{ text: "Связаться с поддержкой" }],
+          [{ text: "Закрыть помощника" }],
+        ],
+      },
+      closeTheKeyboard: {
+        reply_markup: {
+          remove_keyboard: true,
         },
-     
-    }
+      },
+    },
 
+    admin_keyboardСontainer: {
+      parse_mode: "HTML",
 
+      reply_markup: {
+        keyboard: [
+          [{ text: "و عليكم السلام ورحمة الله وبركاته" }],
+          [{ text: "Доставка и цены" }],
+          [{ text: "Закрыть помощника" }],
+        ],
+      },
+    },
+  });
 }
 
-
-module.exports = { key }
+module.exports = { key };
